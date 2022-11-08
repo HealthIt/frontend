3d13b57ac17f0acca84785593317cd6924cfb7db
boksooni <8545652@naver.com>
2022-11-09 02:39:45 +0900

    rename: 메인페이지 토글 컴포넌트 분리/#2

---
+++ src/components/common/MainToggle/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ToggleCon = styled.div`
+  margin-top: 18px;
+  width: 1278px;
+  font-size: 24px;
+  line-height: 28px;
+  color: rgb(174, 184, 196);
+`;
