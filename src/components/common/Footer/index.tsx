ad3cc88fe36913a4023a3786a0f9582747ba83b4
boksooni <8545652@naver.com>
2022-11-08 23:28:10 +0900

    design: Footer Components/#5

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import * as S from './style';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <S.Wrapper>
+        <S.Logo>헬스잇</S.Logo>
+        <S.Text>
+          <S.TopText>(주)헬스잇</S.TopText>
+          <S.MidText>
+            <br /> 대표자 전윤제 사업자 등록번호 106-81-86800
+            <br /> 서울 용산구 한강로2가 2-194
+          </S.MidText>
+          <S.BotText>
+            <br /> Copyright©2022 HealthEat All rights reserved.
+          </S.BotText>
+        </S.Text>
+      </S.Wrapper>
+    </div>
+  );
+};
---
+++ src/components/common/Footer/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: rgb(239, 239, 239);
+  width: 100%;
+  height: 300px;
+  display: flex;
+  position: relative;
+`;
+export const Logo = styled.div`
+  font-size: 24px;
+  line-height: 28px;
+  position: absolute;
+  left: 320px;
+  top: 70px;
+`;
+export const Text = styled.div`
+  position: absolute;
+  left: 430px;
+  top: 70px;
+`;
+export const TopText = styled.div`
+  font-size: 16px;
+  line-height: 18px;
+`;
+export const MidText = styled.div`
+  font-size: 13px;
+  line-height: 20px;
+`;
+export const BotText = styled.div`
+  font-size: 13px;
+  line-height: 15px;
+  color: rgb(101, 159, 255);
+`;
