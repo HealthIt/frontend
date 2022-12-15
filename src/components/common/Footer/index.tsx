177ca99044ce9776b134cb7756b73e1826b6d71a
boksooni <8545652@naver.com>
2022-12-16 02:48:42 +0900

    design: Footer Components/#20

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -5,16 +5,11 @@ export const Footer = () => {
   return (
     <div>
       <S.Wrapper>
-        <S.Logo>헬스잇</S.Logo>
         <S.Text>
-          <S.TopText>(주)헬스잇</S.TopText>
-          <S.MidText>
-            <br /> 대표자 전윤제 사업자 등록번호 106-81-86800
-            <br /> 서울 용산구 한강로2가 2-194
-          </S.MidText>
-          <S.BotText>
-            <br /> Copyright©2022 HealthEat All rights reserved.
-          </S.BotText>
+          <div>Contact Us</div>
+          <div>대표자 전윤제 | yoon22095@gmail.com</div>
+          <div>헬스 잇 | Prod By. SWYG</div>
+          <div>© 2022 by Healtheat All rights reserved.</div>
         </S.Text>
       </S.Wrapper>
     </div>
