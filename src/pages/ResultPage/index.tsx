789b5f1a78c0f9aa036627c860ea57f91bcf8c26
boksooni <8545652@naver.com>
2022-12-28 00:54:31 +0900

    feat: ResultPage/#17

--- src/pages/ResultPage/index.tsx
+++ src/pages/ResultPage/index.tsx
@@ -1,5 +1,47 @@
-import React from 'react';
-
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ResultBar } from '../../components/bmi/ResultBar';
+import { ResultComment } from '../../components/bmi/ResultComment';
+import { ResultInfo } from '../../components/bmi/ResultInfo';
+import { ResultMenu } from '../../components/diet/ResultMenu';
+import * as S from './style';
+//로컬스토리지에 비엠아이 및 정보 값이 없으면 접근 불가
 export const ResultPage = () => {
-  return <div>ResultPage</div>;
+  const bmi = localStorage.BMI;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (bmi === undefined) {
+      alert('결과를 보기위해서는 먼저 bmi계산이 필요합니다!');
+      navigate('/bmi');
+    }
+  }, []);
+
+  return (
+    <div>
+      <S.Wrapper>
+        <div></div>
+        <ResultInfo />
+        <ResultBar />
+        <div>
+          <ResultComment />
+        </div>
+        <S.BMIDiet>
+          <div>
+            {localStorage.Name} 님을 위해 <br />
+            준비했어요!
+          </div>
+          <div>
+            BMI 결과값에 맞춘 <span>추천 식단 레시피</span>
+          </div>
+        </S.BMIDiet>
+        <ResultMenu />
+        <S.ImgWrapper>
+          <div></div>
+          <div></div>
+          <div></div>
+        </S.ImgWrapper>
+      </S.Wrapper>
+    </div>
+  );
 };
---
+++ src/pages/ResultPage/style.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > div:first-child {
+    margin: 70px 0;
+    width: 206px;
+    height: 188px;
+    background-image: url('https://user-images.githubusercontent.com/91241596/208525886-a70a6a08-cd68-4e4c-8d9b-034194f06535.png');
+  }
+`;
+export const BMIDiet = styled.div`
+  /* background-color: lavenderblush; */
+  margin-top: 70px;
+  text-align: center;
+  > div:first-child {
+    margin-bottom: 16px;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 36px;
+    color: #191919;
+  }
+  > div:nth-child(2) {
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 22px;
+    color: #191919;
+    > span {
+      color: #77ae59;
+    }
+  }
+`;
+export const ImgWrapper = styled.div`
+  /* background-color: pink; */
+  position: absolute;
+  display: flex;
+  > div:first-child {
+    background-image: url('https://user-images.githubusercontent.com/91241596/209669551-f942af7b-02b6-4740-a013-fc32feaf4a2b.png');
+    width: 180px;
+    height: 180px;
+    position: relative;
+    bottom: 100px;
+    left: 850px;
+  }
+  > div:nth-child(2) {
+    background-image: url('https://user-images.githubusercontent.com/91241596/209669817-cd7f6575-4fbc-4861-8bde-a98290d89765.png');
+    width: 180px;
+    height: 180px;
+    position: relative;
+    top: 250px;
+    right: 650px;
+  }
+  > div:nth-child(3) {
+    background-image: url('https://user-images.githubusercontent.com/91241596/209669909-2f75bde4-08a9-4dae-afe2-80031ba91956.png');
+    width: 180px;
+    height: 180px;
+    position: relative;
+    top: 950px;
+    left: 550px;
+  }
+`;
