17ef38b31c9b368aca129597acede23092880263
boksooni <8545652@naver.com>
2022-12-30 12:13:19 +0900

    format: LoadingPage 최종수정/#28

--- src/pages/LoadingPage/index.tsx
+++ src/pages/LoadingPage/index.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Footer } from '../../components/common/Footer';
+import { Header } from '../../components/common/Header';
 import * as S from './style';
 
 export const LoadingPage = () => {
+  const navigation = useNavigate();
+  const loading = () => {
+    setTimeout(() => {
+      navigation('/result');
+    }, 4000);
+  };
+  useEffect(() => {
+    loading();
+    return () => {
+      console.log('페이지 로딩중');
+    };
+  });
+
   return (
-    <S.Container>
-      <S.Wrapper>
-        <div>
-          {'사용자 '}님의 BMI 결과를 <br />
-          계산하고 있어요
-        </div>
-        <div>잠시만 기다려주세요</div>
-        <S.Circle />
-        <S.Tomatos />
-      </S.Wrapper>
-    </S.Container>
+    <>
+      <Header />
+      <S.Container>
+        <S.Wrapper>
+          <div>
+            {localStorage.Name} 님의 BMI 결과를 <br />
+            계산하고 있어요
+          </div>
+          <div>잠시만 기다려주세요</div>
+          <div></div>
+        </S.Wrapper>
+      </S.Container>
+      <Footer />
+    </>
   );
 };
