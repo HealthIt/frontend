5ebad1ac6cfb125051ebd53e2233c5a278047799
boksooni <8545652@naver.com>
2022-12-30 12:13:02 +0900

    format: ErrorPage 최종수정/#28

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as S from './style';
 import { useNavigate } from 'react-router-dom';
+import { Header } from '../../components/common/Header';
+import { Footer } from '../../components/common/Footer';
 
 export const ErrorPage = () => {
   const navigate = useNavigate();
@@ -8,13 +10,17 @@ export const ErrorPage = () => {
     navigate('/');
   };
   return (
-    <S.Container>
-      <S.Wrapper>
-        <div></div>
-        <div>죄송합니다. 현재 페이지는 찾을 수 없습니다.</div>
-        <div>요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요 :)</div>
-        <S.Btn onClick={home}>홈으로 이동</S.Btn>
-      </S.Wrapper>
-    </S.Container>
+    <>
+      <Header />
+      <S.Container>
+        <S.Wrapper>
+          <div></div>
+          <div>죄송합니다. 현재 페이지는 찾을 수 없습니다.</div>
+          <div>요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요 :)</div>
+          <S.Btn onClick={home}>홈으로 이동</S.Btn>
+        </S.Wrapper>
+      </S.Container>
+      <Footer />
+    </>
   );
 };
