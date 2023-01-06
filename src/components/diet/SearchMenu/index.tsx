d4de2062e6466164e6d16ff403aa9343e14ba2dc
boksooni <8545652@naver.com>
2023-01-06 22:47:27 +0900

    feat: 메뉴 검색 기능 구현/#28

--- src/components/diet/SearchMenu/index.tsx
+++ src/components/diet/SearchMenu/index.tsx
@@ -1,32 +1,42 @@
 /* eslint-disable no-unused-vars */
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { RecoMenu } from '../RecoMenu';
 import * as S from './style';
 
+interface Sinter {
+  onSearchMenu: () => void;
+}
+
 export const SearchMenu = () => {
-  const bmi = localStorage.BMI;
   const [searchMenu, setSearchMenu] = useState<any[]>([]);
 
   async function getSearch() {
     try {
-      axios
-        .get(`http://52.78.0.222/foods/v1/?irdntNm=&bmi=${bmi}&page=0&size=43`)
-        .then((response) => {
-          setSearchMenu(response.data.data.content);
-        });
+      axios.get(`http://52.78.0.222/foods/v1/?irdntNm=&bmi=&page=0&size=43`).then((response) => {
+        setSearchMenu(response.data.data.content);
+      });
       console.log(searchMenu);
     } catch (error) {
       console.log(error);
     }
   }
+  useEffect(() => {
+    getSearch();
+  }, []);
+  //검색어 로컬스토리지 저장하여
+  const searchData = localStorage.searchData;
+  //음식카드 배열 필터링
+  const test = searchMenu.filter((name) => name.foodNm.includes(searchData));
 
-  const searchList = searchMenu.map((name) => {
+  const searchList = test.map((name) => {
     return (
-      <>
-        <S.FoodWrapper key={name.id}>
+      <S.FoodWrapper key={name.id}>
+        <Link to={`/detail/${name.id}`}>
           <div>
             <img
-              src={name.foodDesc}
+              src={name.img}
               alt={name.foodNm}
               width='290px'
               height='220px'
@@ -34,15 +44,16 @@ export const SearchMenu = () => {
             />
           </div>
           <S.FoodNm>{name.foodNm}</S.FoodNm>
-          <S.FoodDesc>{name.img}</S.FoodDesc>
-        </S.FoodWrapper>
-      </>
+          <S.FoodDesc>{name.foodDesc}</S.FoodDesc>
+        </Link>
+      </S.FoodWrapper>
     );
   });
   return (
     <>
-      <button onClick={() => getSearch()}>test</button>
-      <S.Fo>{searchList}</S.Fo>
+      {/* <button onClick={() => getSearch()}>test</button> */}
+      <S.FoodContainer>{searchList}</S.FoodContainer>
+      {/* <RecoMenu /> */}
     </>
   );
 };
